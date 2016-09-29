9701c2c6fec1b67b9f3d75758ce7b92a48bd06e8
Alberto Perez <aperez@alea-soluciones.com>
2016-09-29 17:41:45 +0200

    Fizz case->Red. Generalize when number is  divisible by 3

--- specs/fizzbuzz_spec.py
+++ specs/fizzbuzz_spec.py
@@ -29,7 +29,7 @@ with describe('FizzBuzz Kata'):
                 expect(result).to(equal(2))
 
         with context('when given number is 3'):
-            with it('returns the given number 3'):
+            with it('returns Fizz'):
                 a_number = 3
 
                 result = fizzbuzzkata.apply_fizz_to(a_number)
@@ -43,3 +43,11 @@ with describe('FizzBuzz Kata'):
                 result = fizzbuzzkata.apply_fizz_to(a_number)
 
                 expect(result).to(equal(a_number))
+
+        with context('when a number is divisible by 3'):
+            with it('returns Fizz'):
+                a_number = 6
+
+                result = fizzbuzzkata.apply_fizz_to(a_number)
+
+                expect(result).to(equal('Fizz'))
