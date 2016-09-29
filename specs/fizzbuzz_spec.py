7b628605e4ead8ce07e5540760e1bdbce7c31361
Alberto Perez <aperez@alea-soluciones.com>
2016-09-29 17:50:55 +0200

    Fizz case->Green. Refactor tests. Generalize

--- specs/fizzbuzz_spec.py
+++ specs/fizzbuzz_spec.py
@@ -14,16 +14,14 @@ with describe('FizzBuzz Kata'):
     with context('Fizz case -> when a number is divisible by 3'):
         with context('when a number is not divisible by 3'):
             with it('returns the same number'):
-                a_number = 7
+                for a_number in [1, 2, 4, 5, 7, 8]:
+                    result = fizzbuzzkata.apply_fizz_to(a_number)
 
-                result = fizzbuzzkata.apply_fizz_to(a_number)
-
-                expect(result).to(equal(a_number))
+                    expect(result).to(equal(a_number))
 
         with context('when a number is divisible by 3'):
             with it('returns Fizz'):
-                a_number = 6
+                for a_number in [3, 6, 9]:
+                    result = fizzbuzzkata.apply_fizz_to(a_number)
 
-                result = fizzbuzzkata.apply_fizz_to(a_number)
-
-                expect(result).to(equal('Fizz'))
+                    expect(result).to(equal('Fizz'))
