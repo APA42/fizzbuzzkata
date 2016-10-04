516e58960fe4d277c01153914f8f61b7bb1e1338
Alberto Perez <aperez@alea-soluciones.com>
2016-10-04 19:14:28 +0200

    FizzBuzz case->Green. When number is divisible by 5 and 3

--- fizzbuzzkata/fizzbuzzkata.py
+++ fizzbuzzkata/fizzbuzzkata.py
@@ -6,6 +6,8 @@ def say_hello():
 
 
 def apply_fizzbuzz_to(a_number):
+    if (a_number % 3 == 0) and (a_number % 5) == 0:
+        return 'FizzBuzz'
     if (a_number % 3) == 0:
         return 'Fizz'
     elif (a_number % 5) == 0:
