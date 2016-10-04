bfc1faf35c3e90da530a7c4b0274c9cde0ee85a2
Alberto Perez <aperez@alea-soluciones.com>
2016-10-04 19:01:13 +0200

    Fizz case->Green. Refactor text context description

--- specs/fizzbuzz_spec.py
+++ specs/fizzbuzz_spec.py
@@ -15,7 +15,7 @@ with describe('FizzBuzz Kata'):
 
             expect(result).to(equal('Hello'))
 
-    with context('Fizz case -> when a number is divisible by 3'):
+    with context('Fizz case'):
         with context('when a number is not divisible by 3'):
             with it('returns the same number'):
                 for a_number in LIST_NUMBERS_NOT_DIVISIBLE_BY_3:
